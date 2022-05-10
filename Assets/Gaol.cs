f66f7dd894893a3357753f5fc13ab3f0a95d852d
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2022-05-10 14:31:08 +0900

    no message

---
+++ Assets/Gaol.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaol : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Gaol!");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
